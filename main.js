47c3abf8ff12cc832389d9a68b01c0bcc9ef40b3
Prakash7 <prakash8782i@gmail.com>
2022-07-30 12:36:05 +0530

    task

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 var a = document.querySelectorAll(".btn");
-var b = document.querySelectorAll(".up");
-var c = document.querySelectorAll(".qty");
 function calculate() {
     var x=(this.parentNode.previousElementSibling.previousElementSibling.innerText);
     var y=parseInt(this.parentNode.previousElementSibling.firstChild.value);
